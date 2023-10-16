023c4a0da198c8d40a176e7eb3f9295b3554c1de
HtMiau <davig4560@gmail.com>
2023-10-16 17:31:31 -0300

    .

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+
 import './App.css';
 import Header from './componentes/Header'
 import Section1 from './componentes/Section1';
@@ -7,16 +8,19 @@ import Section4 from './componentes/Section4'
 import Section5 from './componentes/Section5'
 import Footer from './componentes/Footer'
 
+
 function App() {
   return (
     <body>
-      <Header/>
-      <Section1/>
-      <Section2/>
-      <Section3/>
-      <Section4/>
-      <Section5/>
-      <Footer/>
+      
+        <Header/>
+        <Section1/>
+        <Section2/>
+        <Section3/>
+        <Section4/>
+        <Section5/>
+        <Footer/>
+      
     </body>
   )
 }
